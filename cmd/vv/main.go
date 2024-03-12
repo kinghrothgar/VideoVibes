9affe90cebb2724bec66abaa4e836f087f9d8fc2
Luke Jolly <lukejamesjolly@gmail.com>
2024-03-12 02:23:51 -0600

    First working version

--- cmd/vv/main.go
+++ cmd/vv/main.go
@@ -1,7 +1,138 @@
 package main
 
-import "fmt"
+import (
+	"image"
+	"image/color"
+	_ "image/jpeg"
+	"image/png"
+	_ "image/png"
+	"log"
+	"math"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	imgsPath := os.Args[1]
+	maxGoroutines, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+	outFrames, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	imgs, err := os.ReadDir(imgsPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(imgs) < outFrames {
+		log.Fatal("Number of frame images less than outFrames")
+	}
+
+	frameColors := make([]color.RGBA, len(imgs))
+
+	var wg = sync.WaitGroup{}
+	guard := make(chan struct{}, maxGoroutines)
+	for i, img := range imgs {
+		guard <- struct{}{} // would block if guard channel is already filled
+		wg.Add(1)
+		imgPath := filepath.Join(imgsPath, img.Name())
+		go func(n int) {
+			setAvgColor(&frameColors, n, imgPath)
+			<-guard
+			wg.Done()
+		}(i)
+	}
+
+	wg.Wait()
+
+	createImg(frameColors, outFrames)
+}
+
+func setAvgColor(frameColors *[]color.RGBA, frame int, imgPath string) {
+	file, err := os.Open(imgPath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	imgSize := img.Bounds().Size()
+
+	var redSum float64
+	var greenSum float64
+	var blueSum float64
+
+	for x := 0; x < imgSize.X; x++ {
+		for y := 0; y < imgSize.Y; y++ {
+			pixel := img.At(x, y)
+			col := color.RGBAModel.Convert(pixel).(color.RGBA)
+
+			redSum += float64(col.R)
+			greenSum += float64(col.G)
+			blueSum += float64(col.B)
+		}
+	}
+
+	imgArea := float64(imgSize.X * imgSize.Y)
+
+	red := uint8(math.Round(redSum / imgArea))
+	green := uint8(math.Round(greenSum / imgArea))
+	blue := uint8(math.Round(blueSum / imgArea))
+
+	(*frameColors)[frame] = color.RGBA{red, green, blue, 0xff}
+}
+
+func colorWindowAvg(frameColors []color.RGBA, window, position int) color.RGBA {
+	frameLen := len(frameColors)
+
+	var redSum float64
+	var greenSum float64
+	var blueSum float64
+
+	startFrame := window * position
+	endFrame := min(startFrame+window, frameLen)
+
+	for i := window * position; i < endFrame; i++ {
+		redSum += float64(frameColors[i].R)
+		greenSum += float64(frameColors[i].G)
+		blueSum += float64(frameColors[i].B)
+	}
+
+	frames := float64(endFrame - startFrame)
+	red := uint8(math.Round(redSum / frames))
+	green := uint8(math.Round(greenSum / frames))
+	blue := uint8(math.Round(blueSum / frames))
+	return color.RGBA{red, green, blue, 0xff}
+}
+
+func createImg(frameColors []color.RGBA, outFrames int) {
+	width := 200
+	height := outFrames
+	window := len(frameColors) / outFrames
+
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{width, height}
+
+	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+
+	// Set color for each pixel.
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			img.Set(x, y, colorWindowAvg(frameColors, window, y))
+		}
+	}
+
+	// Encode as PNG.
+	f, _ := os.Create("image_limited.png")
+	png.Encode(f, img)
 }
